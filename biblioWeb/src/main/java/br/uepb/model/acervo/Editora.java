129530040735d3c84abb8a14e45166f634b84252
Caio Lucena <caiolucena.cc@gmail.com>
2018-03-24 17:15:30 -0300

    Interface, Jornal, JornalDAO, Editora, Revista e RevistaDAO

---
+++ biblioWeb/src/main/java/br/uepb/model/acervo/Editora.java
@@ -0,0 +1,21 @@
+package br.uepb.model.acervo;
+
+public class Editora {
+	
+	int id;
+	String nome;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
