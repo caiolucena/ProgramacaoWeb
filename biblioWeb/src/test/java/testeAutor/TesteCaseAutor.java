65d6682ec0f9f7493312f9a976f57cb6accbc88e
Adalcino Junior <adaljunior.inf@gmail.com>
2018-03-27 13:34:48 -0300

    Correções e adicinando novos testes.

---
+++ biblioWeb/src/test/java/testeAutor/TesteCaseAutor.java
@@ -0,0 +1,75 @@
+package testeAutor;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.uepb.dao.AutorDAO;
+import br.uepb.model.Autor;
+import br.uepb.model.acervo.Midias_Eletronicas;
+
+public class TesteCaseAutor {
+	private Autor autor;
+	private AutorDAO autorDao;
+	
+	@Before
+	public void setup() throws Exception{
+		autor = new Autor();
+		autorDao = new AutorDAO();
+	}
+	
+	@Test
+	public void testeCreateAutor(){
+		autor.setNome("Autor1");
+		assertTrue(autorDao.createAutor(autor));
+		
+		autor = new Autor();
+		assertFalse(autorDao.createAutor(autor));
+		
+		autor.setNome("Autor1");
+		ArrayList<Autor> lista = new ArrayList<Autor>();
+		lista = autorDao.searchAutor(autor);
+		for(Autor a:lista){
+			assertTrue(autorDao.removeAutor(a));
+		}
+	}
+	
+	@Test
+	public void testeRemoveAutor(){
+		autor.setNome("Autor1");
+		assertTrue(autorDao.createAutor(autor));
+		autor.setNome("Autor2");
+		assertTrue(autorDao.createAutor(autor));
+		autor.setNome("Autor3");
+		assertTrue(autorDao.createAutor(autor));
+		
+		autor.setNome("Autor");
+		ArrayList<Autor> lista = new ArrayList<Autor>();
+		lista = autorDao.searchAutor(autor);
+		for(Autor a:lista){
+			assertTrue(autorDao.removeAutor(a));
+		}
+	}
+	
+	@Test
+	public void testeUpdateAutor(){
+		autor.setNome("Autor1");
+		assertTrue(autorDao.createAutor(autor));
+		ArrayList<Autor> lista = new ArrayList<Autor>();
+		lista = autorDao.searchAutor(autor);
+		autor = lista.get(0);
+		autor.setNome("AutorNovo");
+		assertTrue(autorDao.updateAutor(autor));
+		
+		autor.setNome("Autor");
+		lista = new ArrayList<Autor>();
+		lista = autorDao.searchAutor(autor);
+		for(Autor a:lista){
+			assertTrue(autorDao.removeAutor(a));
+		}
+	}
+
+}
---
+++ biblioWeb/src/test/java/testeOrientador/TesteCaseOrientador.java
@@ -0,0 +1,81 @@
+package testeOrientador;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.uepb.dao.OrientadorDAO;
+import br.uepb.model.Orientador;
+
+
+public class TesteCaseOrientador {
+	private Orientador orientador;
+	private OrientadorDAO orientadorDao;
+	
+	@Before
+	public void setup() throws Exception{
+		orientador = new Orientador();
+		orientadorDao = new OrientadorDAO();
+	}
+	
+	@Test
+	public void testeCreateOrientador(){
+		orientador.setNome("Orientador1");
+		orientador.setFormacao("Graduado");
+		assertTrue(orientadorDao.createOrientador(orientador));
+		
+		orientador = new Orientador();
+		assertFalse(orientadorDao.createOrientador(orientador));
+		
+		orientador.setNome("Orientador1");
+		ArrayList<Orientador> lista = new ArrayList<Orientador>();
+		lista = orientadorDao.searchOrientador(orientador);
+		for(Orientador o:lista){
+			assertTrue(orientadorDao.removeOrientador(o));
+		}
+	}
+	
+	@Test
+	public void testeRemoveOrientador(){
+		orientador.setNome("Orientador1");
+		orientador.setFormacao("Graducação");
+		assertTrue(orientadorDao.createOrientador(orientador));
+		orientador.setNome("Orientador2");
+		orientador.setFormacao("PosGraducação");
+		assertTrue(orientadorDao.createOrientador(orientador));
+		orientador.setNome("Orientador3");
+		orientador.setFormacao("Graducação");
+		assertTrue(orientadorDao.createOrientador(orientador));
+		
+		orientador.setNome("Orientador");
+		ArrayList<Orientador> lista = new ArrayList<Orientador>();
+		lista = orientadorDao.searchOrientador(orientador);
+		for(Orientador o:lista){
+			assertTrue(orientadorDao.removeOrientador(o));
+		}
+	}
+	
+	@Test
+	public void testeUpdateOrientador(){
+		orientador.setNome("Orientador1");
+		orientador.setFormacao("Graducação");
+		assertTrue(orientadorDao.createOrientador(orientador));
+		ArrayList<Orientador> lista = new ArrayList<Orientador>();
+		lista = orientadorDao.searchOrientador(orientador);
+		orientador = lista.get(0);
+		orientador.setNome("OrientadorNovo");
+		orientador.setFormacao("PosGraducação");
+		assertTrue(orientadorDao.updateOrientador(orientador));
+		
+		orientador.setNome("Orientador");
+		lista = new ArrayList<Orientador>();
+		lista = orientadorDao.searchOrientador(orientador);
+		for(Orientador o:lista){
+			assertTrue(orientadorDao.removeOrientador(o));
+		}
+	}
+
+}
